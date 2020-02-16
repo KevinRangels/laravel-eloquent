d48866e712c7fa30afbdc0fb80c5868eda6b5565
Kevin Rangel <krangel107@gmail.com>
2020-02-15 23:24:52 -0430

    show posts and videos by users

--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{$user->name}}</title>
+        <title>Usuarios de {{$level->name}}</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
         <!-- Fonts -->
@@ -20,34 +20,10 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12 my-3 pt-3 shadow">
-                        <img src="{{ $user->image->url }}" class="float-left rounded-circle mr-2" alt="">
-                        <h1>{{ $user->name }}</h1>
-                        <h3>{{ $user->email }}</h3>
-                        <p>
-                            <strong>Instagram</strong>: {{$user->profile->instagram}} <br>
-                            <strong>Github</strong>: {{$user->profile->github}} <br>
-                            <strong>Web</strong>: {{$user->profile->web}}
-                        </p>
-                        <p>
-                            <strong>País</strong>: {{ $user->location->country }} <br>
-                            <strong>Level</strong>: @if($user->level)
-                                                    <a href=" {{ route('level', $user->level->id) }} ">{{ $user->level->name }}</a>
-                                                    @else
-                                                    ---
-                                                    @endif <br>
-                        </p>
-                        <hr>
-                        <p>
-                            <strong>Grupos</strong>:
-                            @forelse($user->groups as $group)
-                                <span class="badge badge-primary">{{ $group->name }}</span>
-                            @empty
-                                <em>No pertenece a ningun grupo</em>
-                            @endforelse
-                        </p>
-                        <hr>
+                     
 
-                        <h3>Posts</h3>
+                        <h3>Contenido de usuarios nivel {{ $level->name }}</h3>
+                        <hr>
 
                         <div class="row">
                             @foreach($posts as $post)
@@ -79,7 +55,8 @@
                         </div>
 
 
-                    <h3>Videos</h3>
+                    <h3>Contenido en videos de  usuarios nivel {{ $level->name }}</h3>
+                    <hr>
 
                         <div class="row">
                             @foreach($videos as $video)
--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,25 @@ Route::get('/profile/{id}', function($id){
 	
 
 	return view('profile', [
-		'user' => $user,
-		'posts' => $posts,
+		'user'   => $user,
+		'posts'  => $posts,
 		'videos' => $videos
 	]);
 })->name('profile');
+
+Route::get('/level/{id}', function($id){
+	$level = App\Level::find($id);
+
+	$posts = $level->posts()->with('category', 'image', 'tags')
+	->withCount('comments')->get();
+
+	$videos = $level->videos()->with('category', 'image', 'tags')
+	->withCount('comments')->get();
+	
+
+	return view('level', [
+		'level'  => $level,
+		'posts'  => $posts,
+		'videos' => $videos
+	]);
+})->name('level');
